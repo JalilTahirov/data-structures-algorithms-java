1c24ac89e22f921f335a27d9e59af2f4b9722c28
Tahirov, Jalil <jtahirov+deloitte@deloitte.com>
2025-12-27 15:34:23 +0100

    O n square with 2 loops

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("Hello and welcome!"));
-    printItems(5);
-
-
+    printSquare(10);
 }
-
 
 void printItems(int n){
     for(int i = 0; i <= n; i++){
         IO.println(i);
     }
-
     for(int i = 0; i<= n; i++){
         System.out.println(i);
     }
 }
 
-
+void printSquare(int n){
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++) {
+            System.out.println(i + " " + j);
+        }
+    }
+}
