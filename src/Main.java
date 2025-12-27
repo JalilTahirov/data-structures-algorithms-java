ac5ac32d554d7794f56bfcc615b3a5b13872b88d
Tahirov, Jalil <jtahirov+deloitte@deloitte.com>
2025-12-27 15:24:30 +0100

    twoloops

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,20 @@ void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("Hello and welcome!"));
+    printItems(5);
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+
+}
+
+
+void printItems(int n){
+    for(int i = 0; i <= n; i++){
+        IO.println(i);
+    }
+
+    for(int i = 0; i<= n; i++){
+        System.out.println(i);
     }
 }
+
+
